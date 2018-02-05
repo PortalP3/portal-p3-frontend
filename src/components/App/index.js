63be10963f4adee617c7d094a45b31db9a1adb1e
Alexandre Goedert <agoedert@thoughtworks.com>
2018-02-05 17:35:26 -0300

    NO-STORY [Juanca/Alexandre]: Commit inicial para el P3 frontend

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.scss'
+
+const App = () => {
+  return (
+    <div className="app">
+      Hello P3 World!
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ["babel-polyfill", path.resolve('./src/index.js')],
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'portal-p3-frontend-[hash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new StyleLintPlugin({files: 'src/**/*.scss'}),
+    new HtmlWebpackPlugin({template: 'src/index.html'}),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENVIRONMENT': JSON.stringify('DEV')
+      }
+    })
+  ]
+};
