7f57a5a42b83f2ab461c09203ad799aaef7015be
Alexandre Goedert <agoedert@thoughtworks.com>
2018-02-13 18:17:27 -0300

    #128 [Alexandre/JC] Pruebas de article

--- test/components/Article/article.test.js
+++ test/components/Article/article.test.js
@@ -8,6 +8,8 @@ import reducers from '../../../redux/reducers/Reducers'
 
 const article1 = {
   id: '1',
+  date: '2018-02-02T14:19:11',
+  modified: '2018-02-04T14:19:11',
   title: {
     rendered: 'title1'
   },
@@ -18,6 +20,8 @@ const article1 = {
 
 const article2 = {
   id: '2',
+  date: '2018-02-02T14:19:11',
+  modified: '2018-02-04T14:19:11',
   title: {
     rendered: 'title2'
   },
@@ -26,7 +30,19 @@ const article2 = {
   }
 }
 
+const authorInfo = {
+  data: [{
+    name: 'AuthorTest'
+  }]
+}
+
 const articles = [article1, article2]
+
+const wordpressClient = {
+  getAuthorInfo: (author) => {
+    return Promise.resolve(authorInfo)
+  }
+}
 
 const store = createStore(reducers)
 let wrapper
@@ -35,9 +51,25 @@ beforeEach(() => {
   store.dispatch({type: 'CATEGORY_SET_ARTICLES', payload: articles})
   store.dispatch({type: 'ARTICLE_SET_CONTENT', payload: article1})
 
-  wrapper = mount(<Provider store={store}><Article /></Provider>)
+  wrapper = mount(<Provider store={store}><Article wordpressClient={wordpressClient} /></Provider>)
 })
 
 test('render outer div for article', () => {
-  expect(wrapper.find('.article')).toHaveLength(1)
+  expect(wrapper.find('.article-container').find('.article')).toHaveLength(1)
+})
+
+test('render title for article', () => {
+  expect(wrapper.find('.article-container').find('.article').find('h1').text()).toEqual('title1')
+})
+
+test('render article meta', () => {
+  let articleMeta = wrapper.find('.article-container').find('.article').find('.article-meta')
+  expect(articleMeta.find('span').at(0).text()).toEqual('Fecha de creación: 2/2/2018')
+  expect(articleMeta.find('span').at(1).text()).toEqual('Fecha de modificación: 4/2/2018')
+  expect(articleMeta.find('span').at(2).text()).toEqual('Autor: AuthorTest')
+})
+
+test('render article html content', () => {
+  let articleContent = wrapper.find('.article-container').find('.article')
+  expect(articleContent.text()).toMatch(/excerpt1/)
 })
