67562877b5b99cb14f019b59fd8c52056682b6b7
Alexandre Goedert <agoedert@thoughtworks.com>
2018-02-13 17:45:59 -0300

    #128 [Alexandre/JC] Se arreglan pruebas de article

--- test/components/Article/article.test.js
+++ test/components/Article/article.test.js
@@ -6,33 +6,34 @@ import {createStore} from 'redux'
 import Article from '../../../src/components/Article/Article'
 import reducers from '../../../redux/reducers/Reducers'
 
-const articles = [
-  {
-    id: '1',
-    title: {
-      rendered: 'title1'
-    },
-    content: {
-      rendered: 'excerpt1'
-    }
+const article1 = {
+  id: '1',
+  title: {
+    rendered: 'title1'
   },
-  {
-    id: '2',
-    title: {
-      rendered: 'title2'
-    },
-    content: {
-      rendered: 'excerpt2'
-    }
+  content: {
+    rendered: 'excerpt1'
   }
-]
+}
+
+const article2 = {
+  id: '2',
+  title: {
+    rendered: 'title2'
+  },
+  content: {
+    rendered: 'excerpt2'
+  }
+}
+
+const articles = [article1, article2]
 
 const store = createStore(reducers)
 let wrapper
 
 beforeEach(() => {
   store.dispatch({type: 'CATEGORY_SET_ARTICLES', payload: articles})
-  store.dispatch({type: 'CATEGORY_SET_ARTICLE_CONTENT', payload: '1'})
+  store.dispatch({type: 'ARTICLE_SET_CONTENT', payload: article1})
 
   wrapper = mount(<Provider store={store}><Article /></Provider>)
 })
