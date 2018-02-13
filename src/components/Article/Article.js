fce50cc9a7958a92417ca58b290b5f57a9edc525
Alexandre Goedert <agoedert@thoughtworks.com>
2018-02-13 12:32:31 -0300

    #128 [Alexandre/JC] Se agregan estilos básicos a artículos

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,9 +3,15 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import renderHTML from 'react-render-html';
 
+import './articleContainer.scss'
+import './article.scss'
+
 const Article = props => (
-  <div className="article">
-    {renderHTML(props.content.content.rendered)}
+  <div className="article-container">
+    <div className="article">
+      <h1>{props.content.title.rendered}</h1>
+      {renderHTML(props.content.content.rendered)}
+    </div>
   </div>
 )
 
