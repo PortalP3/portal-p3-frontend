5f990eaf1637744534a76b2fd4eb4c817b28bb71
joanca <jcaraven@thoughtworks.com>
2018-02-28 12:33:23 -0300

    #134 [JC] Se configura logger para ser utilizado sólo en desarrollo, se desactiva en Producción

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ import logger from 'redux-logger'
 import reducers from '../redux/reducers/Reducers'
 import App from './components/App/App'
 
+const development = process.env.ENVIRONMENT === 'DEV'
+let store
+
+if(development) {
+  store = createStore(reducers, applyMiddleware(logger))
+} else {
+  store = createStore(reducers)
+}
+
 const root = document.getElementById("root");
 
-console.log(process.env.ENVIRONMENT)
-
 ReactDom.render(
-  <Provider store={createStore(reducers, applyMiddleware(logger))}>
+  <Provider store={store}>
     <App />
   </Provider>
   ,
