cacb981487ea90c4eae55f9f93663d33c5a9d501
Javier Villalobos A <jvillalo@thoughtworks.com>
2018-03-06 18:06:40 -0300

    T136 [Javier/JC] Se crea componente Star para mostrar rating de artículos, aún sin el ícono de estrellas

---
+++ src/components/Rating/Star.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Star = (props) => {
+  let isVoted = Math.round(props.rating) >= props.value ? true : false
+  return (
+    <input 
+      name={props.id}
+      type="radio"
+      checked={isVoted}
+      disabled={props.voted} 
+      onClick={(value) => {
+        props.onClick(value)
+        console.log(value.target.value)
+      }} 
+      value={props.value}
+    />
+)}
+
+Star.propTypes = {
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  voted: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+export default Star
