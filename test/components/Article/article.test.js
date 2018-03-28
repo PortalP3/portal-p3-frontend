f98ab727fbe07dbfc44d29bb4f929eda2ec175d2
Javier Villalobos A <jvillalo@thoughtworks.com>
2018-03-28 12:11:03 -0300

    T153 [Daniel/Javier] Se agrega condición para carga de categorías y artículos si estas no existen

--- test/components/Article/article.test.js
+++ test/components/Article/article.test.js
@@ -37,13 +37,102 @@ const article2 = {
   author_name: 'author2'
 }
 
+const articlesForRequest = {
+  data: [
+    {
+      id: 1,
+      date: '2018-02-02T14:19:11',
+      modified: '2018-02-04T14:19:11',
+      author: 1,
+      title: {
+        rendered: 'title1'
+      },
+      excerpt: {
+        rendered: 'excerpt1'
+      },
+      content: {
+        rendered: 'excerpt1'
+      },
+      author_name:	"twadmin",
+      post_meta_fields: {
+        _edit_last: ["1"],
+        _edit_lock: ["1521054697:1"],
+        rating: [1.0],
+        votes: [2]
+      }
+    },
+    {
+      id: 2,
+      date: '2018-02-02T14:19:11',
+      modified: '2018-02-04T14:19:11',
+      author: 2,
+      title: {
+        rendered: 'title2'
+      },
+      excerpt: {
+        rendered: 'excerpt2'
+      },
+      content: {
+        rendered: 'excerpt2'
+      },
+      author_name:	"twadmin",
+      post_meta_fields: {
+        _edit_last: ["1"],
+        _edit_lock: ["1521054697:1"],
+        rating: [1.0],
+        votes: [2]
+      }
+    }
+  ]
+}
+
+const categories = {
+  data: [{
+    id: 1,
+    name: 'category1',
+    acf: {
+      image: {
+        url: 'image1'
+      }
+    }
+  },
+  {
+    id: 2,
+    name: 'category2',
+    acf: {
+      image: {
+        url: 'image2'
+      }
+    }
+  }]
+}
+
 const articles = [article1, article2]
+
+const wordpressClient = {
+  getArticlesByCategory: (categoryId) => {
+    return Promise.resolve(articlesForRequest)
+  },
+  getCategories: () => {
+    return Promise.resolve(categories)
+  },
+  ratePost: (post, value) => {
+    return Promise.resolve({
+      data: {
+        rating: "2.4",
+        votes: 3
+      }
+    })
+  }
+}
+
 
 const store = createStore(reducers)
 let wrapper
 
 beforeEach(() => {
   store.dispatch({type: 'HEADER_SET_TITLE', payload: 'CATEGORY 1'})
+  store.dispatch({type: 'CATEGORY_LOAD_ALL', payload: categories.data})
   store.dispatch({type: 'CATEGORY_SET_ARTICLES', payload: articles})
   store.dispatch({type: 'ARTICLE_SET_CONTENT', payload: article1})
 
@@ -94,3 +183,25 @@ test('render rating component', () => {
   expect(wrapper.find('.article-container').find('.article').find('Rating')).toHaveLength(1)
 })
 
+
+test('renders an article when categories and articles are not loaded before', (done) => {
+  function todo(wrapper) {
+    wrapper.update()
+    expect(wrapper.find('.article-container').find('.article').find('Rating')).toHaveLength(1)
+    done()
+  }
+  
+  let store = createStore(reducers)
+
+  let wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Article categoryId={1} articleId={1} wordpressClient={wordpressClient} />
+      </MemoryRouter>
+    </Provider>
+  )
+
+  setTimeout(() => {
+    todo(wrapper)
+  }, 0);
+})
