726739594d20becf7ad516016170a4511ed16d77
Javier Villalobos A <jvillalo@thoughtworks.com>
2018-04-03 12:49:24 -0300

    T155 [Daniel/Javier] Se crean los test para las notificaciones en los Ratings

--- test/components/Rating/rating.test.js
+++ test/components/Rating/rating.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {mount, shallow } from 'enzyme'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
+import Notifications from '../../../src/components/Notifications/Notifications'
+import * as Messages from '../../../src/utilities/_messages';
 
 import Rating from '../../../src/components/Rating/Rating'
 import reducers from '../../../redux/reducers/Reducers'
@@ -25,6 +27,7 @@ const wordpressClient = {
 }
 
 const store = createStore(reducers)
+
 
 const wrapper = mount(
   <Provider store={store}>
@@ -53,7 +56,6 @@ test('has an initial state', () => {
   
   expect(rating.state()).toEqual(expectedState)
 })
-
 
 test('state changes when sending vote', async () => {
   let vote = {
@@ -123,4 +125,65 @@ test('change state when receiving new props', () => {
 test('render feedback title', () => {
   let expectedTitle = '¿Qué te pareció el artículo?'
   expect(rating.find('.rating').find('h3').text()).toEqual(expectedTitle)
+})
+
+const getWrapper = (wordpressClient) => {
+  return mount(
+    <Provider store={store}>
+      <div className="article">
+        <Notifications />    
+        <Rating
+          articleMeta={{articleMeta}}
+          articleId={1} 
+          wordpressClient={wordpressClient}
+        />
+      </div>
+    </Provider>)
+}
+  
+test('renders success notification when voting happens', (done) => {
+  function checkNotify(wrapper) {
+    wrapper.update();
+    const expectedValue = Messages.NOTIFICATION_VOTED_ARTICLE_SUCCESS.message
+    const message = wrapper.find(".notification-message").last().text()
+
+    expect(message).toEqual(expectedValue)
+    done()
+  }
+  const wrapper = getWrapper(wordpressClient)
+  const star = wrapper.find('Star').at(0);
+  star.find('input').simulate('click')
+  setTimeout(() => {
+    checkNotify(wrapper)
+  }, 0);
+})
+
+test('renders error notification when voting happens and the user already vote', (done) => {
+  const wordpressClient = {
+    ratePost: (post, value) => {
+      return Promise.resolve({
+        data: {
+          rating: "2.4",
+          votes: 3,
+          error: "Error"
+        }
+      })
+    }
+  }
+
+  function checkNotify(wrapper) {
+    wrapper.update();
+    const expectedValue = Messages.NOTIFICATION_VOTED_ARTICLE_ERROR.message
+    const message = wrapper.find(".notification-message").last().text()
+
+    expect(message).toEqual(expectedValue)
+    done()
+  }
+
+  const wrapper = getWrapper(wordpressClient)
+  const star = wrapper.find('Star').at(0);
+  star.find('input').simulate('click')
+  setTimeout(() => {
+    checkNotify(wrapper)
+  }, 0);
 })
