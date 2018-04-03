ea586fed5a4c18fa5f691e88b0955be870e31df4
Javier Villalobos A <jvillalo@thoughtworks.com>
2018-04-03 16:01:56 -0300

    T155 [Daniel/Javier] Se modifican estilos a las notificaciones

---
+++ src/components/Notifications/NotificationStyle.js
@@ -0,0 +1,23 @@
+const defaultBorder = '3px solid '
+const NotificationStyle = {
+  NotificationItem: { 
+    DefaultStyle: {
+      fontSize: '18px',
+      textAlign: 'center'
+    },
+
+    success: {
+      border: defaultBorder
+    },
+    info: {
+      border: defaultBorder
+    },
+    error: {
+      border: defaultBorder
+    },
+    warning: {
+      border: defaultBorder
+    }
+  }
+}
+export default NotificationStyle
