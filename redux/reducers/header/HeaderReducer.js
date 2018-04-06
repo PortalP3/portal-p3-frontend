6c2ffadbc4fbded612aa14eef879548c029874a7
Daniel Aguilar <daguilar@thoughtworks.com>
2018-04-06 15:38:12 -0300

    T39 [Daniel/Javier] Se muestra la imagen de la categoría como banner en la lista de artículos de esa categoría.

--- redux/reducers/header/HeaderReducer.js
+++ redux/reducers/header/HeaderReducer.js
@@ -1,17 +1,26 @@
+import { HEADER_SET_BACKGROUND, HEADER_SET_TITLE, HEADER_RESET_TITLE } from '../../actions/actionTypes'
+import HEADER_BACKGROUND_URL from '../../../src/config/constants'
+
 const initialValues = {
   title: 'AMAWTA',
-  description: 'Esta plataforma tiene como objetivo el compartir conocimiento crítico que pueda alimentar nuestro compromiso por la justicia social y económica'
+  description: 'Esta plataforma tiene como objetivo el compartir conocimiento crítico que pueda alimentar nuestro compromiso por la justicia social y económica',
+  background: HEADER_BACKGROUND_URL
 }
 
 const HeaderReducer = (state = initialValues, action) => {
   switch (action.type) {
-    case 'HEADER_SET_TITLE':
+    case HEADER_SET_TITLE:
       return {
         ...state,
         title: action.payload,
         description: ''
       }
-    case 'HEADER_RESET_TITLE':
+    case HEADER_SET_BACKGROUND:
+      return {
+        ...state,
+        background: action.payload
+      }  
+    case HEADER_RESET_TITLE:
       return initialValues
     default:
       return state
