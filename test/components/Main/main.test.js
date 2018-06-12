9e5aac29fc10562b4c830512c749d3a7e5e2c9d9
Nicolás Oñate <nonate@thoughtworks.com>
2018-06-12 15:03:08 -0400

    [Nico] refactor en main.test.js para probar con la libreria de google analytics

--- test/components/Main/main.test.js
+++ test/components/Main/main.test.js
@@ -7,34 +7,38 @@ import PageNotFound from '../../../src/components/PageNotFound/PageNotFound';
 
 test('render BrowserRouter', () => {
   const wrapper = shallow(<Main />)
-  expect(wrapper.find('BrowserRouter')).toHaveLength(1)
+
+  console.log('id', wrapper.props());
+
+  expect(wrapper.find('GABrowserRouter')).toHaveLength(1)
 })
 
 test('render route for root', () => {
   const wrapper = shallow(<Main />)
 
-  expect(wrapper.find('BrowserRouter').find('Route').at(0).props().exact).toEqual(true)
-  expect(wrapper.find('BrowserRouter').find('Route').at(0).props().path).toEqual('/')
+  expect(wrapper.find('GABrowserRouter').find('Route').at(0).props().exact).toEqual(true)
+  expect(wrapper.find('GABrowserRouter').find('Route').at(0).props().path).toEqual('/')
 })
 
 test('render route for category', () => {
   const wrapper = shallow(<Main />)
 
-  expect(wrapper.find('BrowserRouter').find('Route').at(1).props().exact).toEqual(true)
-  expect(wrapper.find('BrowserRouter').find('Route').at(1).props().path).toEqual('/category/:categoryId?')
+  expect(wrapper.find('GABrowserRouter').find('Route').at(1).props().exact).toEqual(true)
+  expect(wrapper.find('GABrowserRouter').find('Route').at(1).props().path).toEqual('/category/:categoryId?')
 })
 
 test('render route for article', () => {
   const wrapper = shallow(<Main />)
 
-  expect(wrapper.find('BrowserRouter').find('Route').at(2).props().exact).toEqual(true)
-  expect(wrapper.find('BrowserRouter').find('Route').at(2).props().path).toEqual('/category/:categoryId?/article/:articleId?')
+  expect(wrapper.find('GABrowserRouter').find('Route').at(2).props().exact).toEqual(true)
+  expect(wrapper.find('GABrowserRouter').find('Route').at(2).props().path).toEqual('/category/:categoryId?/article/:articleId?')
 })
 
 test('renders route for PageNotFound', () => {
   const wrapper = shallow(<Main />)
-  expect(wrapper.find('BrowserRouter').find('Route').at(3).props().component).toBeDefined()
-  expect(wrapper.find('BrowserRouter').find('Route').at(3).props().component).toEqual(PageNotFound)
+
+  expect(wrapper.find('GABrowserRouter').find('Route').at(3).props().component).toBeDefined()
+  expect(wrapper.find('GABrowserRouter').find('Route').at(3).props().component).toEqual(PageNotFound)
 })
 
 
